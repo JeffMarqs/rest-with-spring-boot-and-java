351198e32424bb56ddc07a268a7abe4ab9069709
Jefferson Marques <jeffer.son_dasilva@live.com>
2023-12-23 13:04:48 -0300

    Working with verbs

---
+++ rest-with-spring-boot-and-java/src/main/java/br/com/erudio/model/Person.java
@@ -0,0 +1,76 @@
+package br.com.erudio.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Person implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String addresss;
+	private String gender;
+	
+	public Person() {
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddresss() {
+		return addresss;
+	}
+
+	public void setAddresss(String addresss) {
+		this.addresss = addresss;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
