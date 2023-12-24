20c428901a7899d141af9f8919de2c0e71017862
Jefferson Marques <jeffer.son_dasilva@live.com>
2023-12-23 22:05:02 -0300

    Migration With Flyway

---
+++ rest-with-spring-boot-and-java/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Travessa Regia, 19', 'Ester', 'Female', 'Marques Azevedo'),
+	(3, 'Rua Principal, 123', 'João', 'M', 'Silva'),
+	(5, 'Avenida Principal, 123', 'Maria', 'F', 'Silva');
